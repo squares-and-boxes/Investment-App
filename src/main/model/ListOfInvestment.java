7b522e0ca90c37ea4a0c7b19add4d2515ea470c8
Jack <therealjackwu@gmail.com>
2024-10-01 13:23:28 -0700

    Adjusted ListOfInvestment class specification, started ListOfInvestment implementation

--- src/main/model/ListOfInvestment.java
+++ src/main/model/ListOfInvestment.java
@@ -11,24 +11,41 @@ public class ListOfInvestment {
 
     // EFFECTS: Initializes an empty list of investments
     public ListOfInvestment() {
+        listOfInvestment = new ArrayList<Investment>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds investment to the list of investments
     public void add(Investment investment) {
+        listOfInvestment.add(investment);
     }
 
     // REQUIRES: key is a type of investment that is already present.]
     // MODIFIES: this
     // EFFECTS: keeps only the investments for which the type of investment and key match
     public void filter(String key) {
+        for (Investment i : listOfInvestment) {
+            if (!(i.getName().equals(key))) {
+                listOfInvestment.removeInvestment(i);
+            }
+        }
     }
 
+    private List<Investment> removeInvestment() {
+        return ;
+    }
 
     // REQUIRES: key is an investment name that is already present.
     // MODIFIES: this
     // EFFECTS: deletes investments for which the investment name and key match
     public void delete(String key) {
+        int size = listOfInvestment.size();
+
+        for (int i = 0; i < size; i++) {
+            if (listOfInvestment.get(i).getName().equals(key)) {
+                listOfInvestment.remove(i);
+            }
+        }
     }
 
     // EFFECTS: produces a list of statistics in the order [mean,std,max,min,return]
@@ -36,6 +53,7 @@ public class ListOfInvestment {
     }
 
     public int getNumInvestment() {
+        return listOfInvestment.size();
     }
 
 
