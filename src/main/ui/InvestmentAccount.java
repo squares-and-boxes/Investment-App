456ae255769ecfe2aea90efda05a19f78f313abc
Jack <therealjackwu@gmail.com>
2024-10-02 22:31:25 -0700

    Adjusted Main class, added specification of InvestmentAccount UI class

---
+++ src/main/ui/InvestmentAccount.java
@@ -0,0 +1,55 @@
+package ui;
+
+// Investment manager application
+public class InvestmentAccount {
+
+    // EFFECTS: initializes the app interface
+    public InvestmentAccount() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: runs the interface, dealing with human input
+    private void runAccount() {
+    }
+    
+    // MODIFIES: this
+    // EFFECTS: processes human commands
+    private void handleCommand(String c) {
+    }
+
+    // EFFECTS: displays options
+    private void displayOptions() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: delete an investment, with confirmation
+    private void doDelete() {
+    }
+
+    // MODIFIES: thiS
+    // EFFECTS: filter investments
+    private void doFilter() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: display statistics
+    private void doStats() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: view all investments
+    private void doView() {
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add investment
+    private void doAddInvestment() {
+    }
+
+
+
+
+
+
+
+}
