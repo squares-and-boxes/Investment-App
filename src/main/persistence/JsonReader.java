2c62739e6b6b39179902e5ea818cae4dee1288cc
Jack <therealjackwu@gmail.com>
2024-10-19 18:26:35 -0700

    Added Implementation for JsonReader

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -17,31 +17,56 @@ public class JsonReader {
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
+        this.source = source;
     }
 
     // EFFECTS: reads list of investments from file and returns it
     // throws IOException if error occurs during reading
     public ListOfInvestment read() throws IOException {
-        return null;
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseListOfInvestment(jsonObject);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return null;
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses list of investment from JSON object and returns it
     private ListOfInvestment parseListOfInvestment(JSONObject jsonObject) {
-        return null;
+        ListOfInvestment loi = new ListOfInvestment();
+        addInvestments(loi, jsonObject);
+        return loi;
     }
 
     // MODIFIES: loi
     // EFFECTS: parses investments from JSON object and adds them to loi
     private void addInvestments(ListOfInvestment loi, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("investments");
+        for (Object json : jsonArray) {
+            JSONObject nextObject = (JSONObject) json;
+            addInvestment(loi, nextObject);
+        }
     }
+    
 
     // MODIFIES: loi
     // EFFECTS: parses investment from JSON object and adds to loi
     private void addInvestment(ListOfInvestment loi, JSONObject jsonObject) {
+        String type = jsonObject.getString();
+        String name = jsonObject.getString();
+        int amount = jsonObject.getInt();
+        int expRet = jsonObject.getInt();
+        String date = jsonObject.getString();
+        
+        Investment i = new Investment(type,name,amount,expRet,date);
+        loi.addInvestment(i);
     }
 }
